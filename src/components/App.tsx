e0ffc8edf92ae56338c52d2e1cb81c3ebaaecfde
mjvinti <mjvinti@gmail.com>
2022-02-11 12:44:25 -0500

    rendering a list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,15 +5,28 @@ import { fetchTodos, Todo } from '../actions';
 import { StoreState } from '../reducers';
 
 interface AppProps {
-  todos: Todo[];
-  fetchTodos(): any
+    todos: Todo[];
+    fetchTodos(): any
 }
 class _App extends Component<AppProps> {
-  render() {
-    return (
-      <div>Hi there!</div>
-    );
-  }
+    onButtonClick = (): void => {
+        this.props.fetchTodos();
+    }
+
+    renderList(): JSX.Element[] {
+        return this.props.todos.map((todo: Todo) => {
+            return <div key={todo.id}>{todo.title}</div>
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.onButtonClick}>Fetch</button>
+                {this.renderList()}
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
